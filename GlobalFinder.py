e1f65a8567dbda5dc3db2334598c010ab5ded2f5
Resike <resike@gmail.com>
2018-05-14 10:47:32 +0200

    GlobalFinder update

--- GlobalFinder.py
+++ GlobalFinder.py
@@ -1,7 +1,6 @@
 from SublimeLinter.lint import Linter
 
 class GlobalFinder(Linter):
-	syntax = 'lua'
 	cmd = 'luacheck - --formatter=plain --std none --only [111,112,113] --no-unused --no-redefined --no-unused-args --no-unused-secondaries --no-self --no-max-line-length --no-max-code-line-length --no-max-string-line-length --no-max-comment-line-length --codes --ranges --filename @'
 
 	version_args = '--help'
@@ -13,6 +12,9 @@ class GlobalFinder(Linter):
 		r'\((?:(?P<error>E\d+)|(?P<warning>W\d+))\) '
 		r'(?P<message>.+)'
 	)
+	defaults = {
+		'selector': 'source.lua, source.lua.wow, source.luae'
+	}
 
 	def split_match(self, match):
 		match, line, col, error, warning, msg, near = super().split_match(match)
