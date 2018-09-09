8de1c5bc48c586d76d0c034cf9077dca72390238
Resike <resike@gmail.com>
2018-09-10 01:09:59 +0200

    Ignore xml comments from linting

--- GlobalFinder.py
+++ GlobalFinder.py
@@ -9,7 +9,7 @@ class GlobalFinder(Linter):
 		r'(?P<message>.+)'
 	)
 	defaults = {
-		'selector': 'source.lua - meta.tag.xml, source.lua.wow - meta.tag.xml, source.luae'
+		'selector': 'source.lua - meta.tag.xml, source.lua.wow - meta.tag.xml - comment.xml, source.luae'
 	}
 
 	def split_match(self, match):
