66689d269faf91eee2d95a89c7fbcaab8d3aea52
Resike <resike@gmail.com>
2019-12-02 23:12:39 +0100

    Syntax fixes

--- GlobalFinder.py
+++ GlobalFinder.py
@@ -2,7 +2,7 @@ from SublimeLinter.lint import Linter
 
 
 class GlobalFinder(Linter):
-	cmd = 'luacheck - --formatter=plain --std none --only [111,112,113] --no-unused --no-redefined --no-unused-args --no-unused-secondaries --no-self --no-max-line-length --no-max-code-line-length --no-max-string-line-length --no-max-comment-line-length --codes --ranges --filename @'
+	cmd = 'luacheck - --formatter=plain --std none --only [111,112,113] --no-unused --no-redefined --no-unused-args --no-unused-secondaries --no-self --no-max-line-length --no-max-code-line-length --no-max-string-line-length --no-max-comment-line-length --codes --ranges --filename ${file}'
 	regex = (
 		r'^.+:(?P<line>\d+):(?P<col>\d+)\-(?P<col_end>\d+): '
 		r'\((?:(?P<error>E\d+)|(?P<warning>W\d+))\) '
